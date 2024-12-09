51482081f9c7ce9dfbfdaa1757724133bbd05042
Peter Bilberg <peter.langbo.bilberg.beumer@gmail.com>
2024-12-09 21:21:43 +0100

    Started project

---
+++ src/test/java/com/springBootMVC_tutorial/tutorial/MVC/Spring/boot/TutorialMvcSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBootMVC_tutorial.tutorial.MVC.Spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialMvcSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
