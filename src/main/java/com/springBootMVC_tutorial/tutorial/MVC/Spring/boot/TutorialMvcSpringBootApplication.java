6c340fdc9f5ba985700e4755036b2e67e101032d
Peter Bilberg <peter.langbo.bilberg.beumer@gmail.com>
2024-12-09 21:55:44 +0100

    First getMethod

--- src/main/java/com/springBootMVC_tutorial/tutorial/MVC/Spring/boot/TutorialMvcSpringBootApplication.java
+++ src/main/java/com/springBootMVC_tutorial/tutorial/MVC/Spring/boot/TutorialMvcSpringBootApplication.java
@@ -1,13 +1,25 @@
 package com.springBootMVC_tutorial.tutorial.MVC.Spring.boot;
 
+import com.springBootMVC_tutorial.tutorial.MVC.Spring.boot.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class TutorialMvcSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TutorialMvcSpringBootApplication.class, args);
+	}
+
+	@GetMapping
+	public List<Student> hello(){
+		return List.of(new Student(1L,"Peter","Peter@gmail.com", LocalDate.of(2024,10,25),21));
 	}
 
 }
